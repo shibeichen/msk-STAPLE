17f684a1e62642eeaccc15a562d99701d236fece
shibeichen <69106744+shibeichen@users.noreply.github.com>
2022-10-12 15:17:25 +1000

    update

--- _new_test/geo_analysis_fem.m
+++ _new_test/geo_analysis_fem.m
@@ -10,12 +10,12 @@ addpath(genpath('../STAPLE/geometry'));
 
 
 % Testing other dataset
-% dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\riad_masterdataset\stl';
+dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\riad_masterdataset\stl';
 
-dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\Martina_data\stl'
+% dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\Martina_data\stl'
 
 % specify the bone being processed 
-fem_file = fullfile(dir, 'TD12_femur_r.stl');
+fem_file = fullfile(dir, 'femur_r_M01.stl');
 
 % using file selection dialog box
 
@@ -31,9 +31,15 @@ fem_file = fullfile(dir, 'TD12_femur_r.stl');
 fem_tri = load_mesh(fem_file);
 
 % visualise 
-trimesh(fem_tri)
+% trimesh(fem_tri)
+pcshow(fem_tri.Points)
 
 % parameters
 side = 'r'
 
-GIBOC_femur(fem_tri,side, 'cylinder')
+% Run geo analysis
+[CS, JCS, FemurBL] = GIBOC_femur(fem_tri,side, 'cylinder')
+
+
+
+
--- _new_test/mesh_visualization.m
+++ _new_test/mesh_visualization.m
@@ -5,11 +5,23 @@ clear; clc; close all
 addpath(genpath('../STAPLE'));
 addpath(genpath('../STAPLE/geometry'));
 
-test_dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\riad_masterdataset\stl';
-filedir = fullfile (test_dir,'femur_r_M01.stl')
+% import from dir path
+% test_dir = 'E:\OneDrive - Griffith University\____Current_Project_data\_Project_2_KNEE\_Keypoint_ID\femur_geometric_test\femur\riad_masterdataset\stl';
+% filedir = fullfile (test_dir,'femur_r_M01.stl')
+
+% import from file 
+[file,path] = uigetfile('*.stl','import a stl file');
+filedir = fullfile (path,file)
 fem = load_mesh(filedir);
 
-trimesh (fem)
+% Rotate Martina's data into conventional coordinate axis ( x - meidal
+% lateral, y - anterior posterior, z superior and inferior)
 
-% three axis of the point clouds
-x = pc(:,1); y = pc(:,2); z = pc(:,3);
+% Plotting 
+
+% pcshow 
+pcshow(fem.Points)
+
+% scatter3 
+% for a point cloud p, plot in {} mm, in {} color
+% scatter3(p(:,1),p(:,2),p(:,3),0.1,'r')
