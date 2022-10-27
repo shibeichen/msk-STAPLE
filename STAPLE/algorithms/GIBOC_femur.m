65ae6abe2a4bab5cf60c1025e1a2be6c7115c406
shibeichen <69106744+shibeichen@users.noreply.github.com>
2022-10-27 13:22:52 +1000

    save posterior condyle articular surface (femur)

--- STAPLE/algorithms/GIBOC_femur.m
+++ STAPLE/algorithms/GIBOC_femur.m
@@ -160,6 +160,10 @@ if nargout>3
     ArtSurf.(['hip_', side_raw])       = FemHeadTri;
     ArtSurf.(['med_cond_', side_raw])  = fullCondyle_Med_Tri;
     ArtSurf.(['lat_cond_', side_raw])  = fullCondyle_Lat_Tri;
+    %%% changes
+    ArtSurf.(['post_med_cond_', side_raw])  = fullCondyle_Med_Tri;
+    ArtSurf.(['post_lat_cond_', side_raw])  = fullCondyle_Lat_Tri;
+    %%% changes
     ArtSurf.(['dist_femur_', side_raw])= DistFemTri;
     ArtSurf.(['condyles_', side_raw])  = TriUnite(fullCondyle_Med_Tri, fullCondyle_Lat_Tri);
 end
